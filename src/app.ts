bc2aec2dea86a8ed10da6c5eb6791ac66dae671b
Eddy Jo <eddyjo@Eddys-MacBook-Pro.local>
2022-12-06 08:00:43 -0500

    feat: add page component

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('app component works!');
+import { PageComponent } from './components/page.js';
+
+class App {
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+  }
+}
+
+// type assertion - document is not dynamic
+new App(document.querySelector('.document')! as HTMLElement);
