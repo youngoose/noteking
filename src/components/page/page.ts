ac2f48ac502638c2f9317fc360492834e5b52dca
Eddy Jo <eddyjo@Eddys-MacBook-Pro.local>
2022-12-15 08:32:01 -0500

    feat: add new interface and type to have pg item extendable

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -5,9 +5,17 @@ export interface Composable {
 }
 type OnCloseListener = () => void;
 
-class PageItemComponent
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
+
+export class PageItemComponent
   extends BaseComponent<HTMLElement>
-  implements Composable
+  implements SectionContainer
 {
   private closeListener?: OnCloseListener;
   constructor() {
@@ -22,12 +30,14 @@ class PageItemComponent
       this.closeListener && this.closeListener();
     };
   }
+
   addChild(child: Component) {
     const container = this.element.querySelector(
       '.page-item__body'
     )! as HTMLElement;
     child.attachTo(container);
   }
+
   setOnCloseListener(listener: OnCloseListener) {
     this.closeListener = listener;
   }
@@ -36,12 +46,12 @@ export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable
 {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
   }
 
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
     item.setOnCloseListener(() => {
